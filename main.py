1ab92965eee547d703738dd983e83c00cd0202f7
artem1403 <it4idea@gmail.com>
2021-04-23 17:55:09 +0300

    Вынес код в класс, так вроде принято

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import sys
 from PyQt5.QtWidgets import *
 from button_handlers import *
 
-app = QApplication([])
-app.setStyle('Fusion')
-# app.setStyleSheet("QPushButton { margin: 10px; }")
-window = QWidget()
-layout = QVBoxLayout()
-top_button = QPushButton('Top')
-top_button.clicked.connect(on_button_clicked)
-layout.addWidget(top_button)
-layout.addWidget(QPushButton('Bottom'))
-window.setLayout(layout)
-window.show()
-app.exec()
+
+class PyQtLayout(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.UI()
+
+	def UI(self):
+		layout = QVBoxLayout()
+		top_button = QPushButton('Top')
+		top_button.clicked.connect(on_button_clicked)
+		layout.addWidget(top_button)
+		layout.addWidget(QPushButton('Bottom'))
+		self.setLayout(layout)
+		self.show()
+
+
+def main():
+	app = QApplication(sys.argv)
+	app.setStyle('Fusion')
+	ex = PyQtLayout()
+	sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+	main()
